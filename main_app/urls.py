c633acd9cc2cb0e3f5a552d9c181ed8ffe852f88
Amber Taveras <taveras.amber@gmail.com>
2024-10-18 17:02:02 -0400

    implements create household view, adds passcode confirmation to form

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name = 'home'),
+    
+
+    path('household/create', views.HouseholdCreate.as_view(), name = 'household_create')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+from django.views.generic.edit import CreateView
+from django.db import IntegrityError
+from django.contrib import messages
+
+from .models import Household
+from .forms import HouseholdForm
+
 def home (request) :
     return render(request, 'home.html')
+
+class HouseholdCreate (CreateView) :
+    model = Household
+    form_class = HouseholdForm
+    template_name = 'household_form.html'
+
+    def form_valid (self, form) :
+        try :
+            return super().form_valid(form)
+        
+        except IntegrityError :
+            messages.error(self.request, 'Household address already exists. Please try again')
+            return self.form_invalid(form)
+
